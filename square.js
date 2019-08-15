1a55ab1ff5a57308dad12168e50fdd4a22ca0112
Zach Robinson <zrobinson@pivotal.io>
2019-08-15 11:22:07 -0700

    expose test env var

--- square.js
+++ square.js
@@ -1,5 +1,5 @@
 module.exports = x => {
     const xx = x ** 2;
     console.log(`the square of ${x} is ${xx}`);
-    return xx;
+    return ["square result:", xx.toString(), "--", "value of environment variable test:", process.env.test].join(' ');
 }
